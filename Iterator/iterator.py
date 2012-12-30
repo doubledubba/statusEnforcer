6a6b9d68f5b4fd6acd8ca2ef0dc20f6642f5e3f4
Luis Naranjo <luisnaranjo733@gmail.com>
2012-12-29 19:51:51 -0800

    Iterator is working!!!!

--- Iterator/iterator.py
+++
@@ -1,9 +0,0 @@
-from server.models import Computer
-import time
-
-computers = Computer.objects.all()
-for computer in computers:
-	if not computer.withinThreshold():	#if the request is not within the threshold specified, then update the database
-		computer.connected = False
-		computer.status = "ok"
-		computer.save()
