22daef16fb9ebb446bfcaf1edafc4beef0a4747b
Luis Naranjo <luisnaranjo733@gmail.com>
2012-12-30 07:02:24 -0800

    Added logging for iterator

--- statusEnforcer/iterator.py
+++ statusEnforcer/iterator.py
@@ -5,6 +5,11 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'statusEnforcer.settings'
 
 from server.models import Computer
 import time
+import logging
+
+logging.basicConfig(format='[%(levelname)s] [%(asctime)s]: %(message)s',
+        level=logging.INFO)
+logger = logging.getLogger()
 
 while True:
     computers = Computer.objects.all()
@@ -13,6 +18,6 @@ while True:
             computer.connected = False
             computer.status = "ok"
             computer.save()
-            print '%s not within threshold' % computer
+            logger.info('%s not within threshold' % computer)
 
     time.sleep(5)
