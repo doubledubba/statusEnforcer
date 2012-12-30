cad5c494c540378b876e9cf66b59c03c669c9acb
Luis Naranjo <luisnaranjo733@gmail.com>
2012-12-30 02:18:03 -0800

    Reverted to old python script

--- client.py
+++ client.py
@@ -2,32 +2,35 @@ import os
 import requests
 
 CONFIG_PATH = os.path.join(os.environ['HOME'], '.config/statusEnforcer.txt')
-API = 'http://%s/check_in'
+HOST = 'http://10.100.58.69'
+API = '%s/check_in' % HOST
+
+key = 'tester'
 SrvKey = 'fu@qy71q@_2-g_e_!3v$s5ecf)ar=ur0s@t&amp;m5_&amp;fy_elw&amp;m#%'
+
 
 os.system('touch %s' % CONFIG_PATH)
 
-with open(CONFIG_PATH, 'w+') as fh:
-    text = fh.readlines()
-    print text
-    if not text:
+with open(CONFIG_PATH, 'r+a') as fh:
+    config = fh.read()
+    if not config:
         name = raw_input('Name: ')
-        clientKey = raw_input('Personal Key: ')
-        hostName = raw_input('Host name: ')
-        API = API % hostName
-        r = requests.post(API, {'name': name, 'key': clientKey})
-        fh.write(r.text + '\n')
-        fh.write(clientKey + '\n')
-        fh.write(hostName + '\n')
-    else:
-        clientId = text[0]
-        clientKey = text[1]
-        hostName = text[2]
-        API = API % hostName
-        r = requests.post(API, {'clientId': clientId, 'key': clientKey})
-        if r.text == 'nope':
-            print 'Auth failed!'
-        else:
-            status, reqSrvKey = r.text.split('~')
-            if reqSrvKey == SrvKey:
-                print 'Authentic!'
+        r = requests.post(API, {'name': name, 'key': key})
+        print r, r.text
+        fh.write(r.text)
+        
+
+r = requests.post(API, {'clientId': 4, 'key': key})
+
+if r.text == 'nope':
+    print 'Auth failed!'
+
+else:
+    print r.text
+    status, reqSrvKey = r.text.split('~')
+
+    if reqSrvKey == SrvKey:
+        print 'The server is authentic.'
+        print 'Executing:', status
+
+
